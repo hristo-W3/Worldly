0be7f2e937f88919c54ede39aad351a383c4b5d4
Dan Underwood <underwd2@cs.man.ac.uk>
2013-03-09 00:39:08 +0000

    Update returnActiveQuestion

--- returnActiveQuestion.php
+++ returnActiveQuestion.php
@@ -4,24 +4,39 @@ $connection = mysql_connect("ramen.cs.man.ac.uk", "13_COMP10120_W3", "lVgWsvqulk
 mysql_select_db("13_COMP10120_W3", $connection)
     or die('Could not select database');
     
-//$questionToAccess = $_POST['mapNo'];
-$questionToAccess = 1;
+$questionToAccess = $_POST['mapNo'];
+
+$sql = "SELECT QuestionID" 
+       . " FROM Question"
+       . " WHERE Question.Status = 'active'"
+	   . " ORDER BY FIELD(SpecialInfo, 'featured', 'sponsored', 'normal'), QuestionID";
+
+$query = mysql_query($sql);
+
+if($query == false)
+{
+  $questionID = null;
+}
+else
+{
+  $questionID = mysql_result($query, ($questionToAccess - 1));
+} 
 
 $sql = "SELECT Question" 
        . " FROM Question"
-       . " WHERE Question.Status = 'active'";
+       . " WHERE Question.QuestionID ='$questionID'";
        
 $query = mysql_query($sql);
 
-  if($query == false)
-	{
-	  $result = "test";
-	}
-	else
-	{
-	  $result = mysql_result($query, 0);
-	} 
+if($query == false)
+{
+  $question = null;
+}
+else
+{
+  $question = mysql_result($query, 0);
+} 
 
-echo $result;
+echo json_encode(array('questionID' => $questionID, 'question' => $question));
 
 ?>
